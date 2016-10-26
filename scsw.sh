2c67ca4abc8332e9471bcd649bf753d44481a619
Anders <millievv@gmail.com>
2016-10-26 11:51:37 -0700

    added how to tweak alt+tab

--- scsw.sh
+++ scsw.sh
@@ -6,7 +6,7 @@
 LINUX_DIS="$(uname -a | awk -F " " '{print $2}')"
 
 if [ "archlinux" == "$LINUX_DIS" ];then
-    INSTALLCOM="pacman -Syy"
+    INSTALLCOM="sudo pacman -Syyu"
 elif [[ "ubuntu" == "$LINUX_DIS" || "debian" == "$LINUX_DIS" ]];then
     INSTALLCOM="sudo apt-get install"
     INSTALLUPDATE="sudo apt-get update"
@@ -164,32 +164,72 @@ if [ -z "$(echo `which pdflatex`)" ];then
     read latexinstall
 
     if [ "YES" == "$latexinstall" ];then
-        echo " download it from "https://www.tug.org/texlive/acquire-netinstall.html""
-        wget -o $HOME/Downloads/install-tl-unx.tar.gz http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-        tar xvzf $HOME/Downloads/install-tl-unx.tar.gz
-        echo " go to the unzipped directory and install it manually, process will take more than 2 hours!"
-        # add path after installed, add below to /etc/bash.bashrc 
-        #These 3 lines below are related to TEXLIVE (added by YOU on dd/mm/yy)
-        #PATH=/usr/local/texlive/2016/bin/x86_64-linux:$PATH; export PATH
-        #MANPATH=/usr/local/texlive/2016/texmf-dist/doc/man:$MANPATH; export MANPATH
-        #INFOPATH=/usr/local/texlive//2016/texmf-dist/doc/info:$INFOPATH; export INFOPATH 
-        # create /etc/manpath.config if it is not exist and add 
-        #MANPATH_MAP /usr/local/texlive/2015/bin/x86_64-linux /usr/local/texlive/2016/texmf-dist/doc/man 
+        if [ "ArchLinux" == "LINUX_DIS" ];then
+            echo " download it from "https://www.tug.org/texlive/acquire-netinstall.html""
+            wget -o $HOME/Downloads/install-tl-unx.tar.gz http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+            tar xvzf $HOME/Downloads/install-tl-unx.tar.gz
+            echo " go to the unzipped directory and install it manually, process will take more than 2 hours!"
+            ################ ArchLinux
+            # add path after installed, add below to /etc/bash.bashrc 
+            #These 3 lines below are related to TEXLIVE (added by YOU on dd/mm/yy)
+            #PATH=/usr/local/texlive/2016/bin/x86_64-linux:$PATH; export PATH
+            #MANPATH=/usr/local/texlive/2016/texmf-dist/doc/man:$MANPATH; export MANPATH
+            #INFOPATH=/usr/local/texlive//2016/texmf-dist/doc/info:$INFOPATH; export INFOPATH 
+            # create /etc/manpath.config if it is not exist and add 
+            #MANPATH_MAP /usr/local/texlive/2015/bin/x86_64-linux /usr/local/texlive/2016/texmf-dist/doc/man 
+            ############## Ubuntu
+        else # ubuntu
+            $INSTALLCOM texlive-full
+        fi
     fi
 else
     echo "latex live has been installed!\n"
 fi
 
-if [ -z "$(echo `which yed`)" ];then
-    echo "(12) yed graphic editor is not installed in the system, install it (YES)?"
-    read yedinstall
 
-    if [ "YES" == "$yedinstall" ];then
-        echo " download it from http://www.yworks.com/downloads"
+
+if [ -z "$(echo `which java`)" ];then
+    echo "(12) Java  is not installed in the system, install it (YES)?"
+    read javainstall
+
+    if [ "YES" == "$javainstall" ];then
+
+        if [ "archlinux" == "$LINUX_DIS" ];then
+            $INSTALLCOM jdk8-openjdk
+            $INSTALLCOM jre8-openjdk
+        else 
+            echo "no installation find for ubuntu"
+        fi
     fi
 else
     echo "yed has been installed!\n"
 fi
+
+
+if [ -z "$(echo `which yEd`)" ];then
+    echo "(13) yed graphic editor is not installed in the system, install it (YES)?"
+    read yedinstall
+
+    if [ "YES" == "$yedinstall" ];then
+        echo " download it from http://www.yworks.com/,32 or 64 bits\n"
+    fi
+else
+    echo "yed has been installed!\n"
+fi
+
+
+if [ "ArchLinux" == "LINUX_DIS" ];then
+    echo "steps to change behavior of alt+tab"
+    echo "1. terminal-> open "dconf-editor",
+        2. go to org/gnome/desktop/wm/keybindings
+        3. move '<Alt>Tab' from switch-applications to switch-windows
+        4. [optional] move '<Shift><Alt>Tab' from switch-applications-backward to switch-windows-backward
+        5. close "dconf-editor"
+        6. alt+f2, r   to restart gnome" 
+fi
+
+
+
 
 
 
