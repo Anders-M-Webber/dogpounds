3ac801abb02c227827a81498816ce90c43153d25
Anders <millievv@gmail.com>
2018-04-30 11:02:27 -0700

    Update setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 TOP=$HOME
+echo "install vim-gnome to enable copy-clipboard function"
+sudo apt-get install vim-gnome
+
 echo "clone vundle ......"
 git clone https://github.com/VundleVim/Vundle.vim.git  $TOP/.vim/bundle/Vundle.vim
 echo "copy vimrc ......"
